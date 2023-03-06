4d9a7e232cddd358a73627935b0107e809ee396b
saumyagoyal1708 <42496957+saumyagoyal1708@users.noreply.github.com>
2023-03-06 13:43:14 +0530

    backend update - multiple tweets

--- election_analysis_backend/core/views.py
+++ election_analysis_backend/core/views.py
@@ -2,30 +2,58 @@ from django.shortcuts import render
 import csv
 from django.http import JsonResponse
 
-def query_csv(request):
+'''def query_csv(request):
     
     query = request.GET.get('query', None)
-    
+    flag = 0
+    matched_tweets = []
     #print("the query received" + query)
-    if query:
-        flag = 0
-        matched_tweets = []
-        with open('core/Dummy_Tweets.csv', newline='') as csvfile:
+    if query: 
+        filename = 'core/Dummy_Tweets.csv'
+        #with open('core/Dummy_Tweets.csv', newline='') as csvfile:
+        with open(filename, 'r') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 if query in row:
                     flag = 1
                     #response_data = {'result': row}
                     matched_tweets.append(row)
-            if matched_tweets:
+            if flag == 1 :
                 response_data = {'result': matched_tweets}
+        if flag == 0 :
+            response_data = {'result': 'Not Found'}
     else:
         response_data = {'result': 'errorintesting'}
 
-    if flag == 0 :
-        response_data = {'result': 'Not Found'}
     
-    return JsonResponse(response_data)
+    
+    return JsonResponse(response_data)'''
+
+def query_csv(request):
+    query = request.GET.get('query', None)
+    matched_tweets = []
+    if query:
+        filename = 'core/Dummy_Tweets_2.csv'
+        with open(filename, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            flag = 0
+            for row in reader:
+                if query in row:
+                    flag = 1
+                    matched_tweets.append(row)
+
+            if flag == 1 and len(matched_tweets) > 0:
+                response_data = {'result': matched_tweets}
+            else:
+                response_data = {'result': 'Not Found'}
+        return JsonResponse(response_data)
+    else:
+        response_data = {'result': 'errorintesting'}
+        return JsonResponse(response_data)
+
+
+
+
 
 
 # Create your views here.
