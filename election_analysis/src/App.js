6fb0a90dcfceec724f4be6ed54596b1952e20631
saumyagoyal1708 <42496957+saumyagoyal1708@users.noreply.github.com>
2023-03-13 18:13:16 +0530

    frontend updated + column name changed

--- election_analysis/src/App.js
+++ election_analysis/src/App.js
@@ -1,6 +1,12 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
+<<<<<<< Updated upstream
+=======
+import './App.css'
+
+
+>>>>>>> Stashed changes
 
 function QueryForm() {
   const [query, setQuery] = useState('');
@@ -15,8 +21,14 @@ function QueryForm() {
 	//console.log(url)
     try {
       const response = await axios.get(url);
+<<<<<<< Updated upstream
 	  console.log(response)
 	  setResponse(response.data.result);
+=======
+	  console.log(response);
+	  setResponse(response.data.result);
+    setQuery('');
+>>>>>>> Stashed changes
       //setResponse(response.data.result[0][1]);  this is to get value of each column separately 
     } catch (error) {
       console.error(error);
@@ -25,6 +37,7 @@ function QueryForm() {
   };
 
   return (
+<<<<<<< Updated upstream
     <div>
       <form onSubmit={handleSubmit}>
         <label>
@@ -37,6 +50,76 @@ function QueryForm() {
         <p>Response: {response}</p>
       )}
     </div>
+=======
+    <div className='main'>
+      <div className='blue-part'>
+      <div className='search'>
+      <form onSubmit={handleSubmit}>
+        <i className="fa fa-search" aria-hidden="true"></i>
+        <input className="query_inp" placeholder="Write your query here" type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button className="btn" type="submit">Submit</button>
+      </form> 
+      </div>
+      <div className='filter'>
+        <p>Filter 1</p><br></br>
+        <p>Filter 2</p><br></br>
+        <p>Filter 3</p>
+      </div>
+      {/* <div className='response'>
+  {response !== null && (
+    <div>
+      {response.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {row.map((col, colIndex) => (
+            <span key={colIndex}>{col}</span>
+          ))}
+        </div>
+      ))}
+    </div>
+  )}
+</div> */}
+
+      <div className='response'>
+      {response !== null && (
+        <table className='table table-stripped'>
+              <thead className='thead-dark'>
+                <tr>
+                  <th scope='col'>ID</th>
+                  <th scope='col'>Tweet</th>
+                  <th scope='col'>State</th>
+                  <th scope='col'>Sentiment Analysis 1</th>
+                  <th scope='col'>Polarity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {response.map((row, rowindex) => (
+                  <tr key={rowindex}>
+                    {row.map((col, index) => {
+                      let cssClass = '';
+                    if ((index === 3 || index===4) && col === 'Y') {
+                    cssClass = 'positive';
+                     } else if ((index === 3 || index===4) && col === 'N') {
+                      cssClass = 'negative';
+                    }
+                    const columnCssClass = `column-${index + 1}`;
+                    const combinedCssClass = `${columnCssClass} ${cssClass}  `;
+                    
+                      return <td key={index} className={combinedCssClass} >{col}</td>
+                      
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            
+        
+      )}
+    </div>
+      </div>
+      <div className='yellow-part'></div>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
