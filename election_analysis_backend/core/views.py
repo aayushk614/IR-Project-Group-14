1406982d7d7ceb5eeac3fdef06b1d8e98ad9f2fe
saumyagoyal1708 <42496957+saumyagoyal1708@users.noreply.github.com>
2023-03-20 12:23:26 +0530

    backend updated - how to retrieve tweets

--- election_analysis_backend/core/views.py
+++ election_analysis_backend/core/views.py
@@ -32,20 +32,31 @@ from django.http import JsonResponse
 def query_csv(request):
     query = request.GET.get('query', None)
     matched_tweets = []
+    each_tweet = []
+    tweets_list = []
+    column_name = 'Id'
     if query:
-        filename = 'core/Dummy_Tweets_2.csv'
+        filename = 'core/Dummy_Tweets_hashtags1.csv'
         with open(filename, 'r') as csvfile:
-            reader = csv.reader(csvfile)
+            #reader = csv.reader(csvfile)
+            reader = csv.DictReader(csvfile)
             flag = 0
             for row in reader:
-                if query in row:
+                #if query in row:
+                if row[column_name] == query:
                     flag = 1
-                    matched_tweets.append(row)
+                    for key in row:
+                        print(key)
+                        each_tweet.append(row.get(key))
+                    matched_tweets.append(each_tweet)
+                    tweets_list.append(row.get('Tweet'))
+                each_tweet = []
 
             if flag == 1 and len(matched_tweets) > 0:
                 response_data = {'result': matched_tweets}
             else:
                 response_data = {'result': 'Not Found'}
+
         return JsonResponse(response_data)
     else:
         response_data = {'result': 'error'}
