b35f0f357f497aecc2946e63a4043fb3101f0cfe
aayush <aayushk61466@gmail.com>
2023-04-10 01:17:18 +0530

    cluster tweets

---
+++ election_analysis_sentiment/cluster_tweets.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.feature_extraction.text import TfidfVectorizer
+import plotly.express as px
+
+
+st.title("Tweet Clustering")
+
+
+# Define the tweets as a list of strings
+tweets = ["congress made vemula nation talk point even though doubt case amp vemula dalit itself stop bjp make ramalingam case attain nation scale amp chang dalit discours take one remark pm modi make happen",
+"bjp win battl past month bengal pm modi arriv win biggest battl announc warcri upcom war tmc elect modisonarbangla",
+"inclusivemind ark bjpindia rahul gandhi bjp think cellchatumediacroniesscrit writer andbhakt crore speach give victori then late atal bihari vajpeyi much better shine india",
+"senior congress leader manishtewari jpc definit next parliament parallel crimin investig process go go away rafal rafalenot",
+"bc congress take kickback fr pp",
+"rahul lie caught exclus former defenc secretari g mohan kumar tear rahul gandhi lie say no pmo interfer rafal price",
+"expect meltdown congress elect wait till februari end bahut saar possibl hain ji",
+"stori realli expos develop propaganda nation congress cudnt yrs modi sirji ask month abl hear terrifi stori jharkhand govt cudnt even solv basic issu last yrs",
+"issu problem parrikar say defenc secretari resolv problemmatt princip secretari pm actual nail modi anyth els",
+"forthefirsttim activ diplomaci pm modi india emerg leader intern platform first indian prime minist visit israel strengthen tie ageold natur alli year",
+"modiunstopp speech made noisi kharg amp compani nervous confid speak way come easili one sincer amp honest job paplu taplu remain absent perhap nirmala ji arunji piyush ji s trailer prefer avoid modi ji s punch",
+"piti state affair swachh bharat mission modi s kashi could understood fact even minist neelkanth tiwari also admit work mark flopswachhbharat",
+"jim jordan remain best argument drug test member congress whitakerhear",
+"rahul rahul gandhi ji use claim defenc minist manoharparrikar ji complet ignor rafaled note rafal file anoth lie expos rahulliecaught",
+"modi govt make kitchen smoke free pmuy approx crore lpg connect provid poor india becom second largest lpg consum lpg coverag cleaner fuel ensur cleaner environ too transformingindia",
+"bjp mp sh ianuragthakur launch campaign abkibaarpaar",
+"prime minist shri modi today expos congress parti s fals fake campaign sever issu deliv outstand speech floor lok sabha congratul prime minist complet demolish opposit s claim",
+]
+
+
+df = pd.DataFrame({"tweet": tweets})
+
+# Apply K-means clustering
+num_clusters = st.sidebar.slider("Number of clusters", min_value=2, max_value=10, value=5)
+vectorizer = TfidfVectorizer()
+
+
+X = vectorizer.fit_transform(df["tweet"])
+kmeans = KMeans(n_clusters=num_clusters)
+kmeans.fit(X)
+
+# Use PCA to reduce the feature space to 2 dimensions
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X.toarray())
+
+# Add the cluster labels to the DataFrame
+df["cluster"] = kmeans.labels_
+
+# Create an interactive scatter plot using Plotly
+fig = px.scatter(df, x=X_pca[:,0], y=X_pca[:,1], color="cluster", hover_data=["tweet"])
+fig.update_layout(width=800, height=600)
+st.plotly_chart(fig)
+
+
