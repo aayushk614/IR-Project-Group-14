d582f873209e2961a947ec723668cfb89b972a7b
saumyagoyal1708 <42496957+saumyagoyal1708@users.noreply.github.com>
2023-04-22 22:53:35 +0530

    CSV updated and manipulation data added

--- election_analysis_backend/core/views.py
+++ election_analysis_backend/core/views.py
@@ -1,37 +1,9 @@
 from django.shortcuts import render
 import csv
 from django.http import JsonResponse
-
 from .similarity import *
 import numpy as np 
 
-
-'''def query_csv(request):
-    
-    query = request.GET.get('query', None)
-    flag = 0
-    matched_tweets = []
-    #print("the query received" + query)
-    if query: 
-        filename = 'core/Dummy_Tweets.csv'
-        #with open('core/Dummy_Tweets.csv', newline='') as csvfile:
-        with open(filename, 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                if query in row:
-                    flag = 1
-                    #response_data = {'result': row}
-                    matched_tweets.append(row)
-            if flag == 1 :
-                response_data = {'result': matched_tweets}
-        if flag == 0 :
-            response_data = {'result': 'Not Found'}
-    else:
-        response_data = {'result': 'errorintesting'}
-
-    
-    
-    return JsonResponse(response_data)'''
 
 def query_csv(request):
     
@@ -43,7 +15,7 @@ def query_csv(request):
     
     array = [[1.65,2,3,4],[1,3,4,5],[2,4,5,6]]
     if query:
-        filename = 'core/Dummy_Tweets_hashtags3.csv'
+        filename = 'core/Final_Tweet_Dataset.csv'
         with open(filename, 'r') as csvfile:
             #reader = csv.reader(csvfile)
             reader = csv.DictReader(csvfile)
@@ -59,7 +31,7 @@ def query_csv(request):
                     tweets_list.append(row.get('Tweet'))
                 each_tweet = []
 
-
+            
             # replacing the empty entries with N/A
 
             for i in range(0 , len(matched_tweets)) :
@@ -68,17 +40,18 @@ def query_csv(request):
                     if len(matched_tweets[i][j]) == 0 :
                         matched_tweets[i][j] = 'N/A'
             
-            #Finding similarity between fetched tweets (passing tweet_list variable)
-            similarity_matrice = find_similarity_vec(tweet_list = tweets_list)
+            #Finding similarity (pass tweet_list variable)
+            similarity_matrice = find_similarity(tweet_list= tweets_list)
+            import numpy as np
 
-            #Rouding off decimal values to 3DP.
             similarity_matrice_rounded = np.round(similarity_matrice, 3)
 
             idx_col = np.arange(similarity_matrice_rounded.shape[0]).reshape(-1, 1)
             new_similarity_matrice = np.hstack([idx_col+1, similarity_matrice_rounded])
-            new_sim_matrix_list = new_similarity_matrice.tolist()
+            arr1 = new_similarity_matrice.tolist()
+            
 
-            matched_tweets.append(new_sim_matrix_list)
+            matched_tweets.append(arr1)
             
 
             if flag == 1 and len(matched_tweets) > 0:
@@ -94,5 +67,8 @@ def query_csv(request):
 
 
 
+
+
+# Create your views here.
 
 
